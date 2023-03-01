6d89d3915f9925455b9d47fb962fbac63967cfae
graef <simonjn45@gmail.com>
2023-03-01 21:36:29 +0100

    Finished Chapter "Modules"

---
+++ import.hs
@@ -0,0 +1,8 @@
+--------------------
+--    Imports:    --
+--------------------
+
+-- file to show the imports of the Geometry Folder
+import qualified Geometry.Sphere as Sphere
+import qualified Geometry.Cuboid as Cuboid
+import qualified Geometry.Cube as Cube
